e503536a219f31d2c4ff26773395c0dbb11f3628
Delm_ove <Delm_ove@outlook.com>
2022-09-22 16:34:52 +0800

    DMD_submit_1.3

---
+++ assets/web/src/components/RuoYi/Apk/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="phone" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Apk',
+  data() {
+    return {
+      url: '/assets.apk'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
